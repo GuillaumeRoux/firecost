7c72b6f15541a2249ef6858f00b4c4e7d40370eb
Guillaume Roux <guillaume.m.roux@gmail.com>
2012-05-07 11:09:10 +0200

    correction nom variable Signed-off-by: Guillaume Roux <guillaume.m.roux@gmail.com>

--- modify_sale.php
+++ modify_sale.php
@@ -5,7 +5,7 @@
 		
 		if($result = mysql_query($request_sale))
 		{
-			$sql = 'UPDATE ventes SET nom_article="'.$Name.'", decription1="'.$desc.'", decription2="'.$desc2.'", prix="'.$price.'", prix_decr="'.$price_decr.'", nb_decr="'.$nb_decr.'", date_debut="'.$date_start.'", date_fin="'.$date_end.'", heure_debut="'.$hour_start.'", heure_fin="'.$hour_end.'", nb_article="'.$nb.'", rub_id="'.$rub.'", ent_id="'.$ent.'" WHERE vente_id="'.$vente_id.'";';
+			$sql = 'UPDATE ventes SET nom_article="'.$name.'", decription1="'.$desc.'", decription2="'.$desc2.'", prix="'.$price.'", prix_decr="'.$price_decr.'", nb_decr="'.$nb_decr.'", date_debut="'.$date_start.'", date_fin="'.$date_end.'", heure_debut="'.$hour_start.'", heure_fin="'.$hour_end.'", nb_article="'.$nb.'", rub_id="'.$rub.'", ent_id="'.$ent.'" WHERE vente_id="'.$vente_id.'";';
 			$result2 = mysql_query($sql);
 			return true;
 		}
