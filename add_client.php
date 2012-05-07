c77eba38cd6fe3766d68dc016fd5b16e47f808c2
Guillaume Roux <guillaume.m.roux@gmail.com>
2012-05-07 10:30:17 +0200

    Signed-off-by: Guillaume Roux <guillaume.m.roux@gmail.com>

--- add_client.php
+++ add_client.php
@@ -4,6 +4,6 @@
 	$day = $_POST['day'];
 	$year = $_POST['year'];
 	$date = $year."-".$mounth."-".$day;
-	$sql = "INSERT INTO particuliers (prenom,nom_part,adresse_part1,adresse_part2,ville_part,etat_part,postal_part,pays_part,dtnaiss,datemail,pass) VALUES ('".$_POST["fname"]."','".$_POST["lname"]."','".$_POST["adress"]."','".$_POST["adress2"]."','".$_POST["city"]."','".$_POST["state"]."','".$_POST["code"]."','".$_POST["pays"]."','".$date."','".$_POST["mail"]."','".$_POST["password"]."');";
+	$sql = "INSERT INTO particuliers (prenom,nom_part,adresse_part1,adresse_part2,ville_part,etat_part,postal_part,pays_part,dtnaiss,datemail,pass) VALUES ('".$_POST["fname"]."','".$_POST["lname"]."','".$_POST["adress"]."','".$_POST["adress2"]."','".$_POST["city"]."','".$_POST["state"]."','".$_POST["code"]."','".$_POST["country"]."','".$date."','".$_POST["mail"]."','".$_POST["password",'".$_POST["password2"]."');";
                           $sql = mysql_query($sql);
 ?>
